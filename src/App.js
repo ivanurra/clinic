d5e9b0f5779ff490e95533e5a6abf61bcb2f7b86
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-02 18:28:25 +0200

    LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import Cita from "./components/Cita";
 import Formulario from "./components/Formulario";
 
 function App() {
 
+  // Citas en LocalStorage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
   // Array de citas
-  const [ citas, guardarCitas ] = useState([]);
+  const [ citas, guardarCitas ] = useState(citasIniciales);
+
+  // Use Effect para realizar ciertas operaciones cuando el State cambia.
+  useEffect(() => {
+    if(citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas))
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]))
+    }
+  }, [citas] );
 
   // Función que contiene las citas actuales y agrega la nueva
   const crearCita = cita => {
