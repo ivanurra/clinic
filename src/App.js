e20e9313438a38afc9fa8ae22ddbe4bbc59e9348
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-02 18:36:38 +0200

    propTypes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ function App() {
 
   // Use Effect para realizar ciertas operaciones cuando el State cambia.
   useEffect(() => {
+    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+
     if(citasIniciales) {
       localStorage.setItem('citas', JSON.stringify(citas))
     } else {
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import uuid from 'uuid/v4';
+import PropTypes from 'prop-types';
 
 const Formulario = ({crearCita}) => {
     
@@ -114,5 +115,9 @@ const Formulario = ({crearCita}) => {
         </Fragment>
       );
 }
- 
+
+Formulario.propTypes = {
+    crearCita: PropTypes.func.isRequired
+}
+
 export default Formulario;
