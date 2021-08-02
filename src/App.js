b19386b90e667733c6eba5bfdac18325f85ac768
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-02 09:30:07 +0200

    form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <h1>Clínica Dr.Hernández</h1>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
